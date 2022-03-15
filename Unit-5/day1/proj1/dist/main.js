1446f29ec9bdc78449a2ea4d61928ab362224e10
31wizard-ash07 <ashish.singh@mechlintech.com>
2022-03-15 11:34:45 +0530

    day 2 assignments done

---
+++ Unit-5/day1/proj1/dist/main.js
@@ -0,0 +1,1 @@
+(() => { "use strict"; var e = {}; (e => { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) })(e), console.log((0, e.sum)(9, 8)), console.log((0, e.sum)(9, 18)) })();
---
+++ Unit-5/day1/proj1/src/index.js
@@ -0,0 +1,5 @@
+import {sum} from './calc.js'
+console.log(sum(9,8));
+console.log(sum(9, 18));
+
+ 
