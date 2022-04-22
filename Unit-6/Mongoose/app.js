f296ed7fc29566113bc11bc72dfa5464f785a7fc
wizard3107 <ashishsingh9720@gmail.com>
2022-04-22 13:12:13 +0530

    corrected last query

--- Unit-6/Mongoose/app.js
+++ Unit-6/Mongoose/app.js
@@ -1,21 +1,55 @@
-const express = require ('express')
+const express = require('express')
 
 const mongoose = require('mongoose')
 
-const app=express();
-const PORT =8000
+const app = express();
+const PORT = 8000
 
-const connect = ()=>{
+const connect = () => {
     return mongoose.connect("mongodb://localhost:27017/test", {
-        useNewUrlParser:true,
-        useCreateIndex:true,
-        useUnifiedTopology:true
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
     });
 }
 
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    gender: String,
+    ip_address: String,
+    Age: Number
+});
+const movieSchema=new mongoose.Schema({
+    movie_name:String,
+    movie_genre:String,
+    production_year:Number,
+    budget:Number
+})
+
+const User = mongoose.model("user", userSchema);
+
+const Movie = mongoose.model("movie",movieSchema)
+
+// app.get("/users", async (req, res) => {
+//     const user = await User.find({ email: "mini@gmail.com"})
+//     .lean()
+//     .exec()
+//     console.log(user);
+//     res.status(200).json({ data: user })
+// })
+
+app.get("/movies", async (req, res) => {
+    const movie = await Movie.find({})
+        .lean()
+        .exec()
+    console.log(movie);
+    res.status(200).json({ data: movie })
+})
 
 
-const start = async ()=>{
+const start = async () => {
     await connect();
     app.listen(PORT, (req, res) => {
         console.log(`Listening to port : ${PORT}`)
